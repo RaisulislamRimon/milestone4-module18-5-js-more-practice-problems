effbc25513e462ef6fd58463630494699f661ba7
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-03 11:11:10 +0000

    done

---
+++ allPages/jsCode/9.js
@@ -0,0 +1,24 @@
+/* 
+9.
+Write the price of the books that you have. 
+Display the prices if the prices is lower than 200 
+*/
+
+var bookPrice = [
+  20, 40, 60, 80, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
+];
+
+/* for (var i = 0; i < bookPrice.length; i++) {
+  if (bookPrice[i] < 200) {
+    console.log(bookPrice[i]);
+  }
+}
+ */
+console.log(bookPrice.length);
+
+for (var i = 0; i < bookPrice.length; i++) {
+  if (bookPrice[i] > 200) {
+    continue;
+  }
+  console.log(bookPrice[i]);
+}
